ab6ed9bf6e2d0900cb6a188b12454eea14afe546
ewildey93 <wildeye4@gmail.com>
2023-03-27 13:50:09 -0700

    Update FilterDailySpaceUse.R

--- FilterDailySpaceUse.R
+++ FilterDailySpaceUse.R
@@ -1,10 +1,19 @@
 library(dplyr)
 
-Locs3.28 <- read.csv("./3.28Est.Locations.Filter.Distance.187.5_Results.csv")
-table(Locs3.28$TagId)
-str(Locs3.28)
-Locs3.28$DateTime <- as.POSIXct(paste(Locs3.28$Date, Locs3.28$Time), format= "%Y-%m-%d %H:%M:%S", tz="America/Denver")
-Tag781 <- Locs3.28[Locs3.28$TagId == "781E4B78",]
+#read in each Tags locations files and combine into named list
+TagLocs <- list.files(file.path("/scratch/alpine/c834561176@colostate.edu/Locs/"),
+                      pattern="Tag",recursive = TRUE, full.names = TRUE)
+listnames <- list.files(path = "/scratch/alpine/c834561176@colostate.edu/Locs/",pattern = "Tag",recursive=T,full.names = FALSE)
+listnames <- gsub(listnames,pattern = ".rds", replacement = "")
+TagList <- lapply(myfiles, function (x) read_csv(x))
+names(TagList) <- listnames
+#Locs3.28 <- read.csv("./3.28Est.Locations.Filter.Distance.187.5_Results.csv")
+#table(Locs3.28$TagId)
+#str(Locs3.28)
+#Locs3.28$DateTime <- as.POSIXct(paste(Locs3.28$Date, Locs3.28$Time), format= "%Y-%m-%d %H:%M:%S", tz="America/Denver")
+#Tag781 <- Locs3.28[Locs3.28$TagId == "781E4B78",]
+
+#
 DateRangeTag781 <- range(Tag781$Date)
 DateRangeTag781 <- as.Date(DateRangeTag781)
 DateSeqTag781 <- seq.POSIXt(as.POSIXct(paste(DateRangeTag781[1] -1, "05:00:00"),format= "%Y-%m-%d %H:%M:%S", tz="America/Denver"),
@@ -27,6 +36,4 @@ if(nrow(subsetTag) > 0){
   TimeRange <- rbind(TimeRange, x)
 }
 }
-RangeKey <- data.frame(Date=as.Date(), TagId=as.character())
-if(TimeRange$timeDiff > 20) {
-                             RangeKey <- rbind(RangeKey, y)}
+keep <- TimeRange[TimeRange$TimeDiff >= 20,]
