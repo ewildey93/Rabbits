f21c4ac46df0db59ee90fa12eca5c7c880396fe9
ewildey93 <wildeye4@gmail.com>
2023-03-27 21:49:09 -0700

    Just need to generalize filterbyspace the bones are there

--- FilterbySpace.R
+++ FilterbySpace.R
@@ -24,6 +24,9 @@ Locs4 <- lapply(Locs3, function (x) st_as_sf(x, coords = c("x.est", "y.est"), cr
 AvgLocs <- lapply(Locs4, function (x) x%>%summarise(geometry=st_union(geometry))%>%st_centroid)
 AvgLocs <- lapply(AvgLocs, function (x) st_coordinates(x))
 Locs5 <- Map(function (x,y) mutate(x, distance=st_distance(x,y)), Locs4, AvgLocs)
+Locs6 <- rbindlist(Locs5, idcol = TRUE)
+Locs7 <- Locs6%>%group_by(.id)%>%mutate(SDdistance=sd(distance))
+Locs6%>%group_by(.id)%>%summarise(SDdistance=sd(distance))
 ########################scrap###################################
 x <- as.data.frame(t(st_distance(AvgLocs[["781E4B78 01/19-01/20"]], Locs4[["781E4B78 01/19-01/20"]])))
 y <- as.data.frame(Locs5[["781E4B78 01/19-01/20"]][["distance"]])
