53ba753a594f71ede6dae11e41a80763fefac4bc
ewildey93 <wildeye4@gmail.com>
2023-03-27 21:18:40 -0700

    Create FilterbySpace.R

---
+++ FilterbySpace.R
@@ -0,0 +1,28 @@
+library(data.table)
+
+path <- "/scratch/alpine/c834561176@colostate.edu/Rabbits/"
+Locs <- readRDS(paste0(path,"/Locs/TagID.rds"))
+TagID <- unique(Locs$TagId)
+TimeRange <- readRDS(paste0(path, "/TimeRange/TagIDTimeRange.rds"))
+#########################apply time spread filters#########################
+DateNamesList <-paste(TagID, paste0(format(as.Date(keep$DateKeyStart), "%m/%d"), "-", format(as.Date(keep$DateKeyEnd), "%m/%d")))
+Locs2 <- Tag781[-c(1:length(Tag781$TagId)),]
+for (i in 1:length(keep$DateKeyStart)){
+  x <- Tag781[between(Tag781$DateTime, keep$DateKeyStart[i], keep$DateKeyEnd[i]),]
+  x$DateKeyStart <- keep$DateKeyStart[i]
+  Locs2 <- rbind(x, Locs2)
+}
+table(Locs2$Date)
+Locs3 <- split(Locs2, f=Locs2$DateKeyStart)
+names(Locs3) <- DateNamesList
+
+
+#########################filter Locs by point spread########################
+library(sf)
+library(sp)
+Locs4 <- lapply(Locs3, function (x) st_as_sf(x, coords = c("x.est", "y.est"), crs=CRS("+init=epsg:32613")))
+AvgLocs <- lapply(Locs4, function (x) x%>%summarise(geometry=st_union(geometry))%>%st_centroid)
+AvgLocs <- lapply(AvgLocs, function (x) st_coordinates(x))
+Locs5 <- t(mapply(function (x,y) mutate(x, distance=st_distance(x,y)), x=Locs4, y=AvgLocs))
+########################scrap###################################
+
